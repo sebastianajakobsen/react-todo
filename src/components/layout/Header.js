ba28100a9e8c5ed297a0ebaebb5aaeb76aeebfdc
faken09 <seb_jakobsen@live.dk>
2020-08-18 23:22:50 +0200

    wip todo -> added Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <header>
-            <h1>TodoList</h1>
+            <h1 className="p-4 font-black text-2xl">TodoList</h1>
         </header>
     )
 }
